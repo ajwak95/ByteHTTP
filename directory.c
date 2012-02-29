83e98fb8b2a5c57bb34ba4f60af8799e15099571
Alex Conrey <alex@bytepub.us>
2012-02-28 22:02:32 -0600

    added directory.c which _should_ list the contents of a directory [but doesn't work right yet]

---
+++ directory.c
@@ -0,0 +1,20 @@
+#include "httpd.h"
+void directory(DIR *dp, int num)
+{
+	int fd;
+	static char buffer[BUFSIZE*2];
+	  struct dirent *ep;
+	  if (dp != NULL)
+	  {
+		  (void)sprintf(buffer, "<HTML><BODY><H1>Index Listing</H1>");
+
+	    while (ep = readdir(dp))
+	      sprintf(buffer, "%s", ep->d_name);
+
+	      (void) closedir (dp);
+
+	  }
+	  (void)sprintf(buffer, "</BODY></HTML>");
+	  (void)write(num, buffer, strlen(buffer));
+
+}
