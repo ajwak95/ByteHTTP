44d3d029d48f239b455e4e4275339980dc609c0a
Alex Conrey <alex@bytepub.us>
2012-03-04 01:55:51 -0600

    Modified README

--- status.c
+++ status.c
@@ -4,9 +4,11 @@ void status(int type, char *s1, int num)
 	int fd;
 	static char buffer[BUFSIZE*2];
 	switch(type) {
+	case 403: (void)sprintf(buffer, "<HTML><BODY><H1>403 Forbidden</H1></BODY></HTML>");
+	break;
 	case 404: (void)sprintf(buffer, "<HTML><BODY><H1>404 Page Not Found</H1><ADDRESS>ByteHTTPD</ADDRESS></BODY></HTML>");
-	(void)write(num,buffer,strlen(buffer));
 	break;
 	}
+	(void)write(num,buffer,strlen(buffer));
 
 }
